996dc087b33a8409dca1eb557735aba98357b97e
Edge <88197449+Viciousx20@users.noreply.github.com>
2021-09-16 12:57:36 +0700

    Update AUTO HOST MAKER BY EDGE.py

--- AUTO HOST MAKER BY EDGE.py
+++ AUTO HOST MAKER BY EDGE.py
@@ -1,10 +1,10 @@
 import os
 
-os.system("title ARIF TOOLS AUTO HOST MAKER")
+os.system("title Edge TOOLS AUTO HOST MAKER")
 host="host.txt"
 d = open(host, 'w')
-IPv4 = input("Input Your IP : ")
-d.write(f"""{IPv4} growtopia1.com
-{IPv4} growtopia2.com""")
+IP = input("Input Your IP : ")
+d.write(f"""{IP} growtopia1.com
+{IP} growtopia2.com""")
 d.close()
 print("thanks for using this tools now your host has been createed")
